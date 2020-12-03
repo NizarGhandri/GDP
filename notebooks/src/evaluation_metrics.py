dd95c2aaa6d3116a5d9589bbc5277feef0cbf123
ayman mezghani <ayman.mezghani@epfl.ch>
2020-12-03 18:07:39 +0100

    correct implementation

--- notebooks/src/evaluation_metrics.py
+++ notebooks/src/evaluation_metrics.py
@@ -11,8 +11,8 @@ def SST(y: np.ndarray) -> float:
     """
 
     y_avg = np.mean(y)
-    print(y_avg)
-    print(np.square(y - y_avg))
+    # print(y_avg)
+    # print(np.square(y - y_avg))
     return float(np.sum(np.square(y - y_avg)))
 
 
@@ -28,13 +28,6 @@ def M_zero(n: int) -> np.ndarray:
     return np.eye(n) - np.ones((n, n)) / n
 
 
-def SST_test(y: np.ndarray) -> float:
-    n = len(y)
-    y_T = y.reshape((n, 1))
-    # return float(np.matmul(np.matmul(y, M_zero(n)), y_T))
-    return float(y.dot(M_zero(n).dot(y_T)))
-
-
 def SSR(y_hat: np.ndarray) -> float:
     """
     Computes the regression sum of squares.
@@ -43,11 +36,9 @@ def SSR(y_hat: np.ndarray) -> float:
     :return: regression sum of squares
     """
 
-    print(y_hat)
-    n = len(y_hat)
-    y_hat_T = y_hat.reshape((n, 1))
+    n = y_hat.shape[0]
 
-    return float(np.dot(np.dot(y_hat, M_zero(n)), y_hat_T))
+    return float(y_hat.T @ M_zero(n) @ y_hat)
 
 
 def SSE(y: np.ndarray, y_hat: np.ndarray) -> float:
@@ -59,7 +50,7 @@ def SSE(y: np.ndarray, y_hat: np.ndarray) -> float:
     :return: sum of squares errors
     """
     e = y - y_hat
-    print(e)
+    # print(e)
     return float(np.sum(e * e))
 
 
@@ -89,13 +80,3 @@ def adjusted_R_squared(y: np.ndarray, y_hat: np.ndarray, n_features: int) -> flo
     r_squared = R_squared(y, y_hat)
 
     return 1 - (1 - r_squared) * (n - 1) / (n - n_features)
-
-
-def get_beta(X, y):
-    a = np.inv(np.dot(X.T, X))
-    b = np.dot(X.T, y)
-    return np.dot(a, b)
-
-
-l1 = np.array([1, 2, 3, 4, 5])
-# X1 = np.array([])
