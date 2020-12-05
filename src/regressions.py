c614392ec007f5239565157fe04dcf47e476e29e
Kilian Lock <kilianlock@macbook-pro-von-kilian.home>
2020-12-05 08:51:54 +0100

    small changes

--- src/regressions.py
+++ src/regressions.py
@@ -1,30 +1,6 @@
 import numpy as np
 
 
-def regression(dataframe):
-   "normal linear regression"
-   # I delete all na of the data set, find the Real Gross Domestic somewhere and assume it always as our y
-   # I return the beta then
-
-   
-   dataframe=dataframe.dropna();
-   
-   y = dataframe.filter(regex='REAL GROSS DOMESTIC');
-   y = y.values
-   
-   x = dataframe[dataframe.columns.drop(list(dataframe.filter(regex='REAL GROSS DOMESTIC')))];
-   x = x.values
-    
-   oner = np.ones((len(x),1))
-   x=np.hstack((oner,x))
-
-
-   beta = np.dot(np.dot(np.linalg.inv(np.dot(np.transpose(x),x)),np.transpose(x)),y)
-   
-   
-   return beta
-
-## Nizou version 
 def least_squares(X, y):
    """
    Compute least squares with np.solve for more numerical stability 
